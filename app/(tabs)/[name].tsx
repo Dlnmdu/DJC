2ca8813cb4f843d41a34342a62c3bfa128dd4b0c
Dulan Jayarathna <dulanmdu@gmail.com>
2025-08-31 16:17:12 +0530

    First commit

---
+++ app/(tabs)/[name].tsx
@@ -0,0 +1,19 @@
+import { Image, Text, View } from "react-native";
+
+import { useLocalSearchParams } from "expo-router";
+
+export default function HomeScreen() {
+  const { name, email, image } = useLocalSearchParams();
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Image
+        source={{ uri: image }}
+        style={{ width: 200, height: 200, borderRadius: 100 }}
+      />
+      <View style={{ marginTop: 10, alignItems: "center" }}>
+        <Text style={{ fontSize: 20, fontWeight: "500" }}>{name}</Text>
+        <Text>{email}</Text>
+      </View>
+    </View>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,97 @@
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  SignInResponse,
+  SignInSilentlyResponse,
+  isSuccessResponse,
+} from "@react-native-google-signin/google-signin";
+import { useEffect, useState } from "react";
 import { Button, Text, View } from "react-native";
 
 import { useRouter } from "expo-router";
 
 export default function TabTwoScreen() {
   const router = useRouter();
+  const [error, setError] = useState("");
+  const [userInfo, setUserInfo] = useState<
+    SignInResponse | SignInSilentlyResponse
+  >();
+
+  const hasPreviousSignIn = GoogleSignin.hasPreviousSignIn();
+  const currentUser = GoogleSignin.getCurrentUser();
+  const token = GoogleSignin.getTokens();
+
+  console.log("lll user info", userInfo);
+
+  useEffect(() => {
+    GoogleSignin.configure();
+  }, []);
+
+  useEffect(() => {
+    const checkIfSignedIn = async () => {
+      try {
+        const userInfo = await GoogleSignin.signInSilently();
+        setUserInfo(userInfo);
+      } catch {
+        // nothing to do.
+      }
+    };
+
+    checkIfSignedIn();
+  }, []);
+
+  const signIn = async () => {
+    try {
+      const response = await GoogleSignin.signIn();
+      const user = response?.data?.user;
+
+      if (isSuccessResponse(response)) {
+        // you can write this without this method, response.type === "success"
+        router.navigate({
+          pathname: "/(tabs)/[name]",
+          params: {
+            name: user?.name || "",
+            email: user?.email || "",
+            image: user?.photo,
+          },
+        });
+
+        setUserInfo(response);
+        setError("");
+      }
+    } catch (e: any) {
+      setError(e);
+    }
+  };
+
+  const logout = () => {
+    setUserInfo(undefined);
+    GoogleSignin.revokeAccess();
+    GoogleSignin.signOut();
+  };
+
+  console.log("lll userInfo response", userInfo);
+
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text style={{ fontSize: 20, color: "black", textAlign: "center" }}>
         This is first screen
       </Text>
       <Button title="Go to About" onPress={() => router.navigate("/(tabs)")} />
+
+      {error ? <Text>{JSON.stringify(error)}</Text> : null}
+      {userInfo?.data?.idToken ? (
+        <>
+          <Text>{userInfo?.data?.user.name}</Text>
+          <Button title="Logout" onPress={logout} />
+        </>
+      ) : (
+        <GoogleSigninButton
+          size={GoogleSigninButton.Size.Standard}
+          color={GoogleSigninButton.Color.Light}
+          onPress={signIn}
+        />
+      )}
     </View>
   );
 }
